4a1dc3fd9717ba42fe52b7e3c3ac7b70bbdd8ebe
ALARAN IBRAHIM <65920846+ultranet1@users.noreply.github.com>
2021-11-22 03:22:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -20,19 +20,19 @@ def main():
         percent = " %"
 
         if res == 1 and proba <= 20:
-            return render_template('index.html', message = "Something is fishy but HATE SPEECH is far", pred="Prediction probability is ", prob = proba, percent=percent)
+            return render_template('Index.html', message = "Something is fishy but HATE SPEECH is far", pred="Prediction probability is ", prob = proba, percent=percent)
         elif res == 1 and proba <=40:
-            return render_template('index.html', message = "WORD or SENTENCE likely to have HATE SPEECH", pred="Prediction probability is ", prob = proba, percent=percent)
+            return render_template('Index.html', message = "WORD or SENTENCE likely to have HATE SPEECH", pred="Prediction probability is ", prob = proba, percent=percent)
         elif res == 1 and proba <= 60:
-            return render_template('index.html', message = "SLIGHT HATE SPEECH DETECTED!", pred="Prediction probability is ", prob = proba, percent=percent)
+            return render_template('Index.html', message = "SLIGHT HATE SPEECH DETECTED!", pred="Prediction probability is ", prob = proba, percent=percent)
         elif res == 1 and proba <= 80:
-            return render_template('index.html', message = "HATE SPEECH IS DETECTED", pred="Prediction probability is ", prob = proba, percent=percent)
+            return render_template('Index.html', message = "HATE SPEECH IS DETECTED", pred="Prediction probability is ", prob = proba, percent=percent)
         elif res == 1 and proba <= 100:
-            return render_template('index.html', message = "HATE SPEECH IS DETECTED DEFINITELY", pred="Prediction probability is ", prob = proba, percent=percent)
+            return render_template('Index.html', message = "HATE SPEECH IS DETECTED DEFINITELY", pred="Prediction probability is ", prob = proba, percent=percent)
         elif res == 0:
-            return render_template('index.html', message= "No Hate speech is found", pred= "Prediction probability is ", prob = proba, percent=percent)
+            return render_template('Index.html', message= "No Hate speech is found", pred= "Prediction probability is ", prob = proba, percent=percent)
         else:
-            return render_template('index.html', message= "Nothing is found")
+            return render_template('Index.html', message= "Nothing is found")
     history2 = get_history()
     return render_template('Index.html', history2=history2[::-1])
 
